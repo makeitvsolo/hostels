9359c6508641c854ec2d81456d54b3a2f7f0f1d2
makeitvsolo <makeitvsolo@gmail.com>
2023-10-01 13:06:44 +0300

    feat: impl SecurityConfig in `security`

---
+++ src/main/java/com/makeitvsolo/hostels/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.makeitvsolo.hostels.security;
+
+import com.makeitvsolo.hostels.repository.MemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    private final MemberRepository memberRepository;
+
+    public SecurityConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return name -> memberRepository.findByName(name)
+                               .map(member -> new MemberDetails(
+                                       member.getId(),
+                                       member.getName(),
+                                       member.getPassword()
+                               ))
+                               .orElseThrow(() -> new UsernameNotFoundException(name));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        var provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
