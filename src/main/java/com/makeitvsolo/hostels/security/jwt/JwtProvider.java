8c261a2eb54b805de16e9509106d3c951274ec8f
makeitvsolo <makeitvsolo@gmail.com>
2023-10-01 15:11:55 +0300

    feat: impl JwtAuthenticationFilter in `security`

--- src/main/java/com/makeitvsolo/hostels/security/jwt/JwtProvider.java
+++ src/main/java/com/makeitvsolo/hostels/security/jwt/JwtProvider.java
@@ -2,6 +2,8 @@ package com.makeitvsolo.hostels.security.jwt;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public final class JwtProvider {
 
@@ -10,5 +12,9 @@ public final class JwtProvider {
 
     public String encode(String subject) {
         return "";
+    }
+
+    public Optional<String> decodeSubject(String token) {
+        return Optional.empty();
     }
 }
