5cc73b94a4090e7ad650d243896bec1aa7dd3331
makeitvsolo <makeitvsolo@gmail.com>
2023-10-01 18:30:40 +0300

    fix: Member in `model`

--- src/main/java/com/makeitvsolo/hostels/model/Member.java
+++ src/main/java/com/makeitvsolo/hostels/model/Member.java
@@ -5,26 +5,50 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
 @Table(name = "members", indexes = @Index(columnList = "name", unique = true))
 @NoArgsConstructor
-@Getter
 @Setter
 public final class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
     private Long id;
 
     @Column(nullable = false, unique = true)
+    @Getter
     private String name;
 
     @Column(nullable = false)
+    @Getter
     private String password;
+
+    @OneToMany(mappedBy = "owner")
+    private Set<Hostel> hostels;
+
+    @ManyToMany(mappedBy = "tenants")
+    private Set<Hostel> residences;
 
     public Member(String name, String password) {
         this.name = name;
         this.password = password;
+
+        this.hostels = new HashSet<>();
+    }
+
+    public List<Hostel> hostels() {
+        return hostels.stream()
+                       .toList();
+    }
+
+    public List<Hostel> residences() {
+        return residences.stream()
+                       .toList();
     }
 
     @Override
