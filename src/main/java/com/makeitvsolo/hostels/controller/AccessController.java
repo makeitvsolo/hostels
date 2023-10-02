89c8fd11dc4765d20f627862950d2dc682dbc17d
makeitvsolo <makeitvsolo@gmail.com>
2023-10-02 14:52:42 +0300

    fix: AccessController handle exceptions in `controller`

--- src/main/java/com/makeitvsolo/hostels/controller/AccessController.java
+++ src/main/java/com/makeitvsolo/hostels/controller/AccessController.java
@@ -1,8 +1,12 @@
 package com.makeitvsolo.hostels.controller;
 
-import com.makeitvsolo.hostels.service.dto.member.AccessTokenDto;
-import com.makeitvsolo.hostels.service.dto.member.MemberDto;
+import com.makeitvsolo.hostels.controller.response.ErrorMessageDto;
 import com.makeitvsolo.hostels.service.AuthenticationService;
+import com.makeitvsolo.hostels.service.dto.member.MemberDto;
+import com.makeitvsolo.hostels.service.exception.member.MemberAlreadyExistsException;
+import com.makeitvsolo.hostels.service.exception.member.MemberNotFoundException;
+import com.makeitvsolo.hostels.service.exception.member.PasswordsAreNotMatchesException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/access")
 public class AccessController {
@@ -22,17 +27,39 @@ public class AccessController {
 
     @PostMapping("/sign-up")
     public ResponseEntity<?> signUp(@RequestBody MemberDto payload) {
-        authenticationService.register(payload);
+        try {
+            authenticationService.register(payload);
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                       .build();
+            log.info("Member registered");
+            return ResponseEntity.status(HttpStatus.CREATED)
+                           .build();
+        } catch (MemberAlreadyExistsException ex) {
+            log.info(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                           .body(new ErrorMessageDto(ex.getMessage()));
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                           .build();
+        }
     }
 
     @PostMapping("/sign-in")
-    public ResponseEntity<AccessTokenDto> signIn(@RequestBody MemberDto payload) {
-        var token = authenticationService.authenticate(payload);
+    public ResponseEntity<?> signIn(@RequestBody MemberDto payload) {
+        try {
+            var token = authenticationService.authenticate(payload);
 
-        return ResponseEntity.status(HttpStatus.OK)
-                       .body(token);
+            log.info("Member authenticated");
+            return ResponseEntity.status(HttpStatus.OK)
+                           .body(token);
+        } catch (MemberNotFoundException | PasswordsAreNotMatchesException ex) {
+            log.info(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                           .body(new ErrorMessageDto(ex.getMessage()));
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                           .build();
+        }
     }
 }
