4e65a2e475b437080ce39c0212a6775cfbda6d9e
makeitvsolo <makeitvsolo@gmail.com>
2023-10-02 15:58:54 +0300

    fix: cyclic bean dependencies in `security`

--- src/main/java/com/makeitvsolo/hostels/security/jwt/BaseJwtProvider.java
+++ src/main/java/com/makeitvsolo/hostels/security/jwt/BaseJwtProvider.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.makeitvsolo.hostels.service.security.JwtProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
@@ -14,7 +15,10 @@ import java.util.Optional;
 @Service
 public final class BaseJwtProvider implements JwtProvider {
 
+    @Value("${spring.jwt.secret}")
     private String secretKey;
+
+    @Value("${spring.jwt.expirationInMinutes}")
     private Long expirationInMinutes;
 
     public String encode(String subject) {
