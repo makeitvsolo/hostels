893252bf910de7c3b2493be4da75bddeb326a2a4
makeitvsolo <makeitvsolo@gmail.com>
2023-10-02 13:58:26 +0300

    fix: HostelController now handle tenants in `controller`

--- src/main/java/com/makeitvsolo/hostels/controller/HostelController.java
+++ src/main/java/com/makeitvsolo/hostels/controller/HostelController.java
@@ -1,8 +1,10 @@
 package com.makeitvsolo.hostels.controller;
 
+import com.makeitvsolo.hostels.dto.HostelDto;
 import com.makeitvsolo.hostels.dto.HostelItemDto;
 import com.makeitvsolo.hostels.security.MemberPrincipal;
 import com.makeitvsolo.hostels.service.HostelService;
+import com.makeitvsolo.hostels.service.TenantService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,16 +16,18 @@ import java.util.List;
 @RequestMapping("/api/v1/hostels")
 public class HostelController {
 
-    private final HostelService service;
+    private final HostelService hostelService;
+    private final TenantService tenantService;
 
-    public HostelController(HostelService service) {
-        this.service = service;
+    public HostelController(HostelService hostelService, TenantService tenantService) {
+        this.hostelService = hostelService;
+        this.tenantService = tenantService;
     }
 
     @GetMapping
     public ResponseEntity<List<HostelItemDto>> getAll(@AuthenticationPrincipal MemberPrincipal principal) {
         return ResponseEntity.status(HttpStatus.OK)
-                       .body(service.getAll(principal.getId()));
+                       .body(hostelService.getAll(principal.getId()));
     }
 
     @PostMapping
@@ -31,9 +35,42 @@ public class HostelController {
             @RequestBody String name,
             @AuthenticationPrincipal MemberPrincipal principal
     ) {
-        service.create(principal.getId(), name);
+        hostelService.create(principal.getId(), name);
 
         return ResponseEntity.status(HttpStatus.CREATED)
+                       .build();
+    }
+
+    @GetMapping("/{hostelId}/tenants")
+    public ResponseEntity<HostelDto> getAllTenants(
+            @PathVariable("hostelId") Long hostelId,
+            @AuthenticationPrincipal MemberPrincipal principal
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+                       .body(tenantService.getAllTenants(principal.getId(), hostelId));
+    }
+
+    @PostMapping("/{hostelId}/tenants")
+    public ResponseEntity<?> addTenant(
+            @RequestBody String memberName,
+            @PathVariable("hostelId") Long hostelId,
+            @AuthenticationPrincipal MemberPrincipal principal
+    ) {
+        tenantService.addTenant(principal.getId(), hostelId, memberName);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                       .build();
+    }
+
+    @DeleteMapping("/{hostelId}/tenants")
+    public ResponseEntity<?> removeTenant(
+            @RequestBody Long tenantId,
+            @PathVariable("hostelId") Long hostelId,
+            @AuthenticationPrincipal MemberPrincipal principal
+    ) {
+        tenantService.removeTenant(principal.getId(), hostelId, tenantId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT)
                        .build();
     }
 }
